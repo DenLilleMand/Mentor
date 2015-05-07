199ed04bc677344de7ec3883d7bf593fbf289835
Realbp <martinbergpetersen@gmail.com>
2015-05-07 22:07:45 +0200

    Newest update with search function fully working

--- Mentor/Scripts/SearchFunction.js
+++ Mentor/Scripts/SearchFunction.js
@@ -1,12 +1,15 @@
 ﻿function searchForThings() {
     //show the searches
+    mentorCount = 0;
+    personCount = 0;
     var inputSearchText = $("#inputSearch").val();
     $('.searchHeaders').children().remove();
+ 
     // alert('You pressed Seach button');
 
     if (inputSearchText === "") {
         $('.searchField').hide();
-        $('.searchHeaders').children().remove();
+       // $('.searchHeaders').children().remove();
     }
     else {
         var test = { 'input': inputSearchText };
@@ -17,16 +20,20 @@
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
             success: function (data) {
-
+               
                 $('.searchField').show();
 
 
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].FirstName != null) {
-                        $('.Persons').append('<li><a class="outputSearch" href="/User/index/'+data[i].Id+'"><img src="~/pictures/b.png" class="searchPicture"/>' + data[i].FirstName + '</a></li>');
+                        $('.Persons').append('<li><a class="outputSearch" href="/User/index/' + data[i].Id + '"><img src="~/pictures/b.png" class="searchPicture"/>' + data[i].FirstName +' '+ data[i].LastName+ '</a></li>');
+                        personCount++;
+                        $('#noResultPerson').hide();
                     } else {
-                        $('.Interest').append('<li><a class="outputSearch" href=""><img src="~/pictures/b.png" class="searchPicture"/>' + data[i].Name + '</a></li>');
                         
+                        $('.Interest').append('<li><a class="outputSearch" href="/Program/index'+data[i].Id+'"><img src="~/pictures/b.png" class="searchPicture"/>' + data[i].Name + '</a></li>');
+                        mentorCount++;
+                        $('#noResultMentorPrograms').hide();
                     }
 
 
@@ -38,7 +45,12 @@
             }
         });
 
-
+        if (personCount === 0) {
+            $('#noResultPerson').show();
+        }
+        if (mentorCount === 0) {
+            $('#noResultMentorPrograms').show();
+        }
 
     }
 
