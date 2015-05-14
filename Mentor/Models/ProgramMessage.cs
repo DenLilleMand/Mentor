bef2480c7029258b81a0def1dac220c3d406e518
denlillemand <mattinielsen5@hotmail.com>
2015-05-14 11:49:01 +0200

    glemte at tilføje et par nye klasser.

---
+++ Mentor/Models/ProgramMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Mentor.Models
+{
+    public class ProgramMessage
+    {
+        public int Id { get; set; }
+        public int ProgramId { get; set; }
+        public string Message { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set;  }
+
+         
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        [ForeignKey("ProgramId")]
+        public virtual Program Program { get; set; }
+
+    }
+}
